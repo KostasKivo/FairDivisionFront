fefae5f8236b9208e6b4583f42b2912aa4dd3153
Konstantinos Kivotos <kostasss.ryan@gmail.com>
2024-08-13 16:23:40 +0300

    Add instruction message when response is null

--- src/components/Layout/Body.tsx
+++ src/components/Layout/Body.tsx
@@ -4,8 +4,8 @@ import "./Body.css"; // Assuming you have defined styles here
 const Body = ({ response }: { response: any }) => {
     return (
         <div className="body">
-            {response && response.bad ? (
-                <p className="error-message">{response.message}</p>
+            {response === null ? (
+                <h2 className="instruction-message">{"Please select the inputs of the problem using the slider and submit to get the algorithm results"}</h2>
             ) : (
                 <pre>{JSON.stringify(response, null, 2)}</pre>
             )}
